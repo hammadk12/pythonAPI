b79565e66d6e47a62382402f8a40acb4ed6e93a7
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-04-25 12:18:28 -0400

    interpolated variables with user input, validated inputs, & added error handling

--- index.py
+++ index.py
@@ -1,38 +1,96 @@
 import json
 import requests
-import sys
+import re
 
 
-api_url = 'https://api.api-ninjas.com/v1/cars?limit=5&make=lamborghini&year=2024&model=urus'
-api_url2 = 'https://api.api-ninjas.com/v1/cars?limit=5&make=toyota&'
-api_url3 = 'https://api.api-ninjas.com/v1/cars?limit=5&make=toyota&year=2020'
-
-response = requests.get(api_url, headers={'X-Api-Key': '+LNP8mg1hDCqYaUM+C6ElQ==xZ3S7GJWP3oQZBvU'})
-# print(json.dumps(response.json(), indent=2))
-
-def displayMenu():
-    print("Welcome to the Car Information Hub. Search Whatever you would like.")
-
+def displayMenu(make, model, year):
     while True:
-        makeInput = input("Enter make: ")
-        if makeInput == "Toyota":
-            makeResponse = requests.get(api_url2, headers={'X-Api-Key': '+LNP8mg1hDCqYaUM+C6ElQ==xZ3S7GJWP3oQZBvU'})
-            print(json.dumps(makeResponse.json(), indent=2))
-            print("These are some of our", makeInput + "'s")
-            optionResponse = input("Would you like to refine your search with a specific year or model? Enter Y to specify year, M to specify model. ")
-            if optionResponse == "Y".upper():
-                yearInput = input("Enter year of Toyota model: ")
-                if yearInput == "2020":
-                    yearResponse = requests.get(api_url3, headers={'X-Api-Key': '+LNP8mg1hDCqYaUM+C6ElQ==xZ3S7GJWP3oQZBvU'})
-                    print(json.dumps(yearResponse.json(), indent=2))
-                    print("Here are some", yearInput + " " + makeInput + "'s.")
-                    break
-        else:
-            print("Invalid make.")
-        
+        api_url = f'https://api.api-ninjas.com/v1/cars?limit=30&make={make}&model={model}&year={year}'
+        response = requests.get(api_url, headers={'X-Api-Key': '+LNP8mg1hDCqYaUM+C6ElQ==xZ3S7GJWP3oQZBvU'})
 
-def main():
-    displayMenu()
+        if response.status_code == 200:
+            data = response.json()
+            if data: # Check if data list is not empty
+                print("Vehicles: ")
+                print(json.dumps(response.json(), indent=2))
+            else:
+                print("No matches found. Please try different search criteria.")
+                get_car_info()
+        else:
+            print("Failed to retrieve data. Please check your inputs.")
+    
+    
+# validate make input
+def get_valid_make(prompt):
+    while True:
+        make_input = input(prompt)
+
+        if re.match(r"^[A-Za-z ]+$", make_input):
+            return make_input
+        else:
+            print("Invalid make. Please enter valid make, only letters.")
+
+# validate model input
+def get_valid_model(prompt):
+    while True:
+        model_input = input(prompt)
+
+        if re.match(r"^[A-Za-z0-9]+$", model_input):
+            return model_input
+        else:
+            print("Invalid model. Please enter a valid model using only letters and numbers.")
+
+def get_car_info():
+    print("Welcome to the Car Information Hub. Search Whatever you would like.")
+    make = get_valid_make("Enter make: ")
+    model = get_valid_model("Enter model: ")
+    year = input("Enter year: ")
+    return make, model, year
 
 if __name__ == "__main__":
-    main()
+    make, model, year = get_car_info()    
+    displayMenu(make, model, year)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # if makeInput == "Toyota":
+           # makeResponse = requests.get(api_url2, headers={'X-Api-Key': '+LNP8mg1hDCqYaUM+C6ElQ==xZ3S7GJWP3oQZBvU'})
+            #print(json.dumps(makeResponse.json(), indent=2))
+            #print("These are some of our", makeInput + "'s")
+            #optionResponse = input("Would you like to refine your search with a specific year or model? Enter Y to specify year, M to specify model. ")
+            #if optionResponse == "Y".upper():
+                #yearInput = input("Enter year of Toyota model: ")
+                #if yearInput == "2020":
+                    #yearResponse = requests.get(api_url3, headers={'X-Api-Key': '+LNP8mg1hDCqYaUM+C6ElQ==xZ3S7GJWP3oQZBvU'})
+                    #print(json.dumps(yearResponse.json(), indent=2))
+                    #print("Here are some", yearInput + " " + makeInput + "'s.")
+                    #break
+        #else:
+            #print("Invalid make.")
